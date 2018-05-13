febfda3d33c0c7059f9616ef5e699d84488eef18
Unknown <chatajazzy@interia.pl>
2018-05-13 18:38:01 +0200

    Add router and load data at page start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
-import './App.css'; 
+import { Switch, Route } from 'react-router-dom';
+
+import './App.css';
+
+import Header from './components/Header';
+import PhotoGrid from './components/PhotoGrid';
+import Single from './components/Single';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>App</p>
+        <Header />
+        <main>
+          <Switch>
+            <Route exact path="/" component={PhotoGrid} />
+            <Route exact path="/view/:postId" component={Single} />
+          </Switch>
+        </main>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { loadComments } from './actions/commentsActions';
+import { loadPosts } from './actions/postsActions';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import configureStore from './store/configureStore';
+
+const store = new configureStore(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+store.dispatch(loadComments());
+store.dispatch(loadPosts());
+
+const Root = () => {
+  return (
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  );
+};
+
+ReactDOM.render(<Root />, document.getElementById('root'));
